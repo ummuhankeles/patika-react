a87469d120fbe337e25fa07ddeb989785010566a
Ümmühan Keleş <ummuhankeles4@gmail.com>
2021-08-08 17:05:44 +0300

    completed filtering process

--- contacts-app/src/components/contacts/list/Index.js
+++ contacts-app/src/components/contacts/list/Index.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function List({ contacts }) {
+    const [filterText, setFilterText] = useState('');
+
+    const filtered = contacts.filter((item) => {
+        return Object.keys(item).some((key) => 
+            item[key]
+            .toString()
+            .toLowerCase()
+            .includes(filterText.toLocaleLowerCase())
+        )
+    })
+
+    console.log("filter", filtered)
+
     return (
         <div>
+            <input 
+            placeholder="filter contacts"
+            value={filterText}
+            onChange={(e) => setFilterText(e.target.value)}
+            />
+
             <ul>
                 {
-                    contacts.map((contact, index) => (
+                    filtered.map((contact, index) => (
                         <li key={index} > {contact.fullname} </li>
                     ))
                 }
