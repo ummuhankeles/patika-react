bea6da5f87776e470830bc66d54d7ea2eb2574a5
Ümmühan Keleş <ummuhankeles4@gmail.com>
2021-08-10 14:23:16 +0300

    created components

--- odev2-todo/src/App.js
+++ odev2-todo/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Footer from './components/footer/Footer';
+import Todos from './components/todos/Todos';
 
 function App() {
   return (
     <div className="App">
-      
+      <Todos />
+      <Footer />
     </div>
   );
 }
